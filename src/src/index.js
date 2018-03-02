67bac0dcbd7e0ecd156f591ba147e22691c1bb26
Samuel Cha <sunchipssam@gmail.com>
2018-03-02 09:22:09 -0800

    Added redux and react-router boilerplate

--- src/src/index.js
+++ src/src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import Logger from 'redux-logger';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import reducers from './reducers';
+import App from './App';
+
+import './index.css';
+
+const createStoreWithMiddleware = applyMiddleware(ReduxThunk, Logger)(
+  createStore,
+);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Router>
+      <div>
+        <Route path="/" component={App} />
+      </div>
+    </Router>
+  </Provider>,
+  document.getElementById('root'),
+);
---
+++ src/src/reducers/auth.js
@@ -0,0 +1,3 @@
+export default (auth = {}, action) => {
+  return auth;
+};
