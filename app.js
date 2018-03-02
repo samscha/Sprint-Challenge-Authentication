2a98138214b51a22e556b89d41c0378fcb36c8ec
Samuel Cha <sunchipssam@gmail.com>
2018-03-02 08:27:08 -0800

    Added deps (previous commit) and added auth for mongod

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const { server } = require('./server');
 const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
+const { mongodbAuth } = require('./config.js');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/auth-users', {
-  useMongoClient: true
+  useMongoClient: true,
+  user: mongodbAuth.user,
+  pass: mongodbAuth.pass,
+  authSource: mongodbAuth.authSource,
 });
 
 server.listen(port, () => {
--- config.js
+++ config.js
@@ -1,4 +1,10 @@
+const mongodbAuth = {
+  user: 'sam',
+  pass: '1234',
+  authSource: 'admin',
+};
 const mysecret = 'Why can’t banks keep secrets? There are too many tellers!';
 module.exports = {
-  mysecret
+  mysecret,
+  mongodbAuth,
 };
